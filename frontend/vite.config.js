be8badc6c98194aeab7a56343ffd415b4bac25dc
yunetcoderss <danonenothing@gmail.com>
2026-07-08 19:11:52 +0700

    add json file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://sketsa-kediri-ai-chatbot.vercel.app',
         changeOrigin: true,
         secure: false,
       },
